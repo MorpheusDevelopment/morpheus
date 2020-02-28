de5e200f1880cd04ea680c2870ff30e46dff6a30
Autumn Mauzey <morpheus@mauzey.com>
2020-02-28 15:49:01 -0800

    First DAL test which is pretty much bogus.   It should create the data first and then test that it comes back as expected.   I just assume data is coming back as expected.   It's more about setting up the test builder/runner/project references at this point.

---
+++ DataAccessLayer_Tests/RecipesData_Tests.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer;
+using NUnit.Framework;
+using Objects;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer_Tests
+{
+    [TestFixture, Category("RecipeData Tests")]
+    public class RecipesData_Tests
+    {
+        private RecipeData _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new RecipeData();
+        }
+
+        [Test, Category("RecipeData GetSavedRecipes RetrievesExpected")]
+        public async Task RecipeData_GetSavedRecipes_RetrievesExpected()
+        {
+            var expected = new Recipe { Name = "Peanut Butter Cookies" };
+
+            //Get the list of recipes and make sure expectedis there.
+            var savedRecipes = await _target.GetSavedRecipes();
+            var actual = savedRecipes.Value.Where(x => x.Name == expected.Name);
+            Assert.IsNotEmpty(actual);
+        }
+    }
+}
