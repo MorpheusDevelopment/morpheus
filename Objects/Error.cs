abb167eb926f4be2e547c193227b68e27175c08f
Autumn Mauzey <morpheus@mauzey.com>
2020-02-28 14:50:44 -0800

    Adding in the Objects project and the basic Error concept.

---
+++ Objects/Error.cs
@@ -0,0 +1,11 @@
+﻿using Objects.Interfaces;
+using System.Collections.Generic;
+
+namespace Objects
+{
+    public class Error : IError
+    {
+        public string ErrorMessage { get; set; }
+        public IEnumerable<string> Suggestions { get; set; }
+    }
+}
---
+++ Objects/Interfaces/IError.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Objects.Interfaces
+{
+    public interface IError
+    {
+        string ErrorMessage { get; }
+        IEnumerable<string> Suggestions { get; }
+    }
+}
