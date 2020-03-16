98fc8ffadd4906b3610d1deb479f0c31e46dee47
Autumn Mauzey <morpheus@mauzey.com>
2020-03-16 12:06:21 -0700

    I was asked to implement a palindrome.   This was my simple naive implementation without googleing a solution.

---
+++ BusinessImplementationZone/Extenders/StringExtenders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BusinessImplementationZone.Extenders
+{
+    public static class StringExtenders
+    {
+        public static bool SimplePalindromeCheck(this string input)
+        {
+            for (int i = 0; i < Math.Ceiling(((double)input.Length)/2); i++)
+            {
+                if (input[i] != input[(input.Length-1) - i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ BusinessImplementationZone_Tests/Extenders/StringExtenders_Tests.cs
@@ -0,0 +1,18 @@
+﻿using BusinessImplementationZone.Extenders;
+using NUnit.Framework;
+
+namespace BusinessImplementationZone_Tests.Extenders
+{
+    [TestFixture, Category("StringExtenders Tests")]
+    public class StringExtenders_Tests
+    {
+        [TestCase("", true), Category("StringExtenders RecipeDataInterface")]
+        [TestCase("bob", true), Category("StringExtenders RecipeDataInterface")]
+        [TestCase("john", false), Category("StringExtenders RecipeDataInterface")]
+        [TestCase("abba", true), Category("StringExtenders RecipeDataInterface")]
+        public void StringExtenders_SimplePalindromeCheck(string input, bool expectedResult)
+        {
+            Assert.AreEqual(input.SimplePalindromeCheck(), expectedResult);
+        }
+    }
+}
